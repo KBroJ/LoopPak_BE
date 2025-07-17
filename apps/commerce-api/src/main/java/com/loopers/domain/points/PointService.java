1d83b36fe4a089baf7680e313b0c80c34098f1e6
KBroJ <kbroj9210@gmail.com>
2025-07-17 22:39:38 +0900

    feat: 포인트 충전 로직 및 통합 테스트 구현

--- apps/commerce-api/src/main/java/com/loopers/domain/points/PointService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/points/PointService.java
@@ -2,6 +2,8 @@ package com.loopers.domain.points;
 
 import com.loopers.domain.users.UserModel;
 import com.loopers.domain.users.UserService;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +26,19 @@ public class PointService {
 
         return pointInfo;
 
+    }
+
+    public PointModel ChargePoint(String userId, Long point) {
+
+        UserModel userInfo = userService.getMyInfo(userId);
+        if (userInfo == null) {
+            throw new CoreException(ErrorType.NOT_FOUND, "회원정보가 없습니다.");
+        }
+
+        PointModel userPoint = pointRepository.findById(userInfo.getId())
+                .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND, "포인트 정보를 찾을 수 없습니다."));
+
+        return userPoint;
     }
 
 
