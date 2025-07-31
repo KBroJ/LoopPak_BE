a40dbe998ec01db1f847809fbd319fc03bb01bb7
KBroJ <kbroj9210@gmail.com>
2025-07-31 17:18:00 +0900

    feat: 좋아요 통합테스트 - 진행중

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/like/LikeService.java
@@ -0,0 +1,12 @@
+package com.loopers.domain.like;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class LikeService {
+
+
+
+}
