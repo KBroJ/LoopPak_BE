d649adaa563f1b97d25952fc2bee84457ce14799
KBroJ <kbroj9210@gmail.com>
2025-08-12 21:08:19 +0900

    feat: redis 환경 기초 설정

--- apps/commerce-api/build.gradle.kts
+++ apps/commerce-api/build.gradle.kts
@@ -9,6 +9,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${project.properties["springDocOpenApiVersion"]}")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
 
     // querydsl
     annotationProcessor("com.querydsl:querydsl-apt::jakarta")
